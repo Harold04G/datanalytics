80c11bb4642603da43c8a782faa3dab1b43cac40
Harold04G <gonzalezjosua304@gmail.com>
2025-05-21 10:30:26 -0500

    Barras con ploty

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
 
 st.title("Mi primera publicación Streamlit")
 st.header("Introducción")
 st.write("Esta es mi primera publicación en Streamlit.")
 st.subheader("Cambios en la publicación")
+
+
+st.write("Este es un gráfico de barras simple.")
+fig = px.bar(
+    x=["A", "B", "C"],
+    y=[1, 2, 3],
+    title="Gráfico de barras",
+)
+st.plotly_chart(fig)
