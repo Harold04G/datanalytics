e742adc2523434961e66eeb4a7dcbb7535111e3f
Harold04G <gonzalezjosua304@gmail.com>
2025-05-21 10:36:47 -0500

    graficos extra

--- app.py
+++ app.py
@@ -17,3 +17,24 @@ fig = px.bar(
 )
 st.plotly_chart(fig)
 date = st.date_input("Pick a date")
+
+st.write("Este es un gráfico de lineal simple.")
+fig = px.line(
+    x=["A", "B", "C"],
+    y=[1, 2, 3],
+    title="Gráfico de líneas",
+)
+st.plotly_chart(fig)
+st.write("Este es un gráfico de dispersión simple.")
+fig = px.scatter(
+    x=["A", "B", "C"],
+    y=[1, 2, 3],
+    title="Gráfico de dispersión",
+)
+st.plotly_chart(fig)
+st.write("Este es un gráfico de caja simple.")
+fig = px.box(
+    x=["A", "B", "C"],
+    y=[1, 2, 3],
+    title="Gráfico de caja",
+)
