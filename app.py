16f76f5f8126840bca9442d6decdd599bc4f0a36
Harold04G <gonzalezjosua304@gmail.com>
2025-05-21 10:21:19 -0500

    Añadir cambios

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title("Mi primera publicación")
+st.title("Mi primera publicación Streamlit")
 st.header("Introducción")
 st.write("Esta es mi primera publicación en Streamlit.")
+st.subheader("Cambios en la publicación")
